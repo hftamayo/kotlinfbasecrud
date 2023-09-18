ed3f27dc2d372052e49dfbf7c3105dd23302207a
hftamayo <hftamayo@gmail.com>
2023-09-18 10:25:10 -0600

    bindind data from form and cleaning object's content

--- kotlinfbasecrudadmin/src/main/java/com/hftamayo/kotlinfbasecrudadmin/UploadActivity.kt
+++ kotlinfbasecrudadmin/src/main/java/com/hftamayo/kotlinfbasecrudadmin/UploadActivity.kt
@@ -2,10 +2,31 @@ package com.hftamayo.kotlinfbasecrudadmin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.hftamayo.kotlinfbasecrudadmin.databinding.ActivityUploadBinding
 
 class UploadActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityUploadBinding
+    private lateinit var databaseReference: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_upload)
+        binding = ActivityUploadBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.saveButton.setOnClickListener {
+            val ownerName = binding.uploadOwnerName.text.toString()
+            val vehicleBrand = binding.uploadVehicleBrand.text.toString()
+            val vehicleRTO = binding.uploadVehicleRTO.text.toString()
+            val vehicleNumber = binding.uploadVehicleNumber.text.toString()
+
+            databaseReference = FirebaseDatabase.getInstance().getReference("Vehicle Information")
+            val vehicleData = VehicleData(ownerName, vehicleBrand, vehicleRTO, vehicleNumber)
+            databaseReference.child(vehicleNumber).setValue(vehicleData).addOnSuccessListener {
+                binding.uploadOwnerName.text.clear()
+                binding.uploadVehicleBrand.text.clear()
+                binding.uploadVehicleRTO.text.clear()
+                binding.uploadVehicleNumber.text.clear()
+            }
+        }
     }
 }
