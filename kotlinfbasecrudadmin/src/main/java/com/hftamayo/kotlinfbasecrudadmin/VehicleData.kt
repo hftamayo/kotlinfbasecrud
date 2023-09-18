e88165114a93f62a94234f572ebf6bf72061fa54
hftamayo <hftamayo@gmail.com>
2023-09-18 09:59:09 -0600

    markup of vehicle data class

---
+++ kotlinfbasecrudadmin/src/main/java/com/hftamayo/kotlinfbasecrudadmin/VehicleData.kt
@@ -0,0 +1,10 @@
+package com.hftamayo.kotlinfbasecrudadmin
+
+data class VehicleData(
+    val ownerName: String? = null,
+    val vehicleBrand: String? = null,
+    val vehicleRTO: String? = null,
+    val vehicleNumber: String? = null
+){
+
+}
