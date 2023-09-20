b126a06921cdd07ddb8fb521ad59a56850616cdf
hftamayo <hftamayo@gmail.com>
2023-09-20 11:14:27 -0600

    business logic for displaying a record

--- app/src/main/java/com/hftamayo/kotlinfbasecrud/MainActivity.kt
+++ app/src/main/java/com/hftamayo/kotlinfbasecrud/MainActivity.kt
@@ -2,10 +2,35 @@ package com.hftamayo.kotlinfbasecrud
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.hftamayo.kotlinfbasecrud.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var databaseReference: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun readData(vehicleNumber: String){
+        databaseReference = FirebaseDatabase.getInstance().getReference("Vehicle Information")
+        databaseReference.child(vehicleNumber).get().addOnSuccessListener {
+            if (it.exists()){
+                val ownerName = it.child("ownerName").value
+                val vehicleBrand = it.child("vehicleBrand").value
+                val vehicleRTO = it.child("vehicleRTO").value
+                Toast.makeText(this, "Results Found", Toast.LENGTH_SHORT).show()
+                binding.searchVehicleNumber.text.clear()
+                binding.readOwnerName.text = ownerName.toString()
+                binding.readVehicleBrand.text = vehicleBrand.toString()
+                binding.readVehicleRTO.text = vehicleRTO.toString()
+            } else {
+                Toast.makeText(this, "No results found", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
