ed9812045fe9a1f8725c94a6030ec6b263d6a187
hftamayo <hftamayo@gmail.com>
2023-09-22 10:56:13 -0600

    adding and wiring update button

--- kotlinfbasecrudadmin/src/main/java/com/hftamayo/kotlinfbasecrudadmin/MainActivity.kt
+++ kotlinfbasecrudadmin/src/main/java/com/hftamayo/kotlinfbasecrudadmin/MainActivity.kt
@@ -17,5 +17,11 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+
+        binding.mainUpdate.setOnClickListener {
+            val intent = Intent(this@MainActivity, UpdateActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
